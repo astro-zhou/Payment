a5b0df50a95db66ec6cabf4dc0d0db238c223e5c
yuhang <1398540400@qq.com>
2022-11-21 10:39:32 +0800

    微服务提供者支付Module模块

---
+++ cloud-provider-payment8001/src/main/java/com/zhou/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.zhou.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zyh
+ * @create 2022-11-20 18:12
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/zhou/springcloud/controller/PaymentController.java
@@ -0,0 +1,51 @@
+package com.zhou.springcloud.controller;
+
+import com.zhou.springcloud.entities.CommonResult;
+import com.zhou.springcloud.entities.Payment;
+import com.zhou.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zyh
+ * @create 2022-11-21 10:05
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment) {
+
+        int result = paymentService.create(payment);
+        log.info("*****插入结果: " + result);
+
+        if (result > 0) {
+            return new CommonResult(200, "数据库插入成功", result);
+        } else {
+            return new CommonResult(444, "数据库插入失败", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****插入结果: " + payment);
+
+        if (payment != null) {
+            return new CommonResult(200, "查询成功", payment);
+        } else {
+            return new CommonResult(444, "没有对应记录,查询ID: " + id, null);
+        }
+    }
+}
