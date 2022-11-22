16725c375f6e387d9394ec7ccf34d6d5b9339e8d
yuhang <1398540400@qq.com>
2022-11-22 12:10:42 +0800

    EurekaClient端order80注册进EurekaServer成为服务consumer

--- cloud-consumer-order80/src/main/java/com/zhou/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/zhou/springcloud/OrderMain80.java
@@ -2,11 +2,14 @@ package com.zhou.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @author zyh
  * @create 2022-11-21 14:51
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
