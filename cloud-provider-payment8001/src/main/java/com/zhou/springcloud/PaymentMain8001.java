791639cc94103a0b7e698a0a5467be98fd3b143f
yuhang <1398540400@qq.com>
2022-11-22 11:56:36 +0800

    EurekaClient端payment8001注册进EurekaServer成为服务provider

--- cloud-provider-payment8001/src/main/java/com/zhou/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zhou/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.zhou.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zyh
  * @create 2022-11-20 18:12
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
