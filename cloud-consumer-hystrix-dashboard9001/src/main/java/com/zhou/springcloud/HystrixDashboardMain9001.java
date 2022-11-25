6e95854b59ff495990155d9cb29249e42bb5a48c
yuhang <1398540400@qq.com>
2022-11-25 19:46:52 +0800

    hystrix服务监控hystrixDashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/zhou/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.zhou.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zyh
+ * @create 2022-11-25 19:33
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
