7a0854ecb6e1b3356515ca05edc711b36742e095
yuhang <1398540400@qq.com>
2022-11-28 18:12:32 +0800

    Nacos作为配置中心-基础配置

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/zhou/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.zhou.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zyh
+ * @create 2022-11-28 17:22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
